2f709904c62f1e6e22665886a23185a826249b88
DHoops <dmhoops@gmail.com>
2019-08-21 15:14:49 -0400

    duplicate changes made in system

--- Event/WTUA;BUILDING!BUILDING!NEW!NA.js
+++ Event/WTUA;BUILDING!BUILDING!NEW!NA.js
@@ -1,4 +1,8 @@
 if(wfTask == "Review Consolidation" && wfStatus == "Completed"){     
    assessImpactFees();
    addResFees();
+   
+   if(AInfo["Total Floor Area"] > 0){
+	   updateFee('FIREREV', 'BD_PERMITS', 'FINAL', 1, "N");
+   }
 }

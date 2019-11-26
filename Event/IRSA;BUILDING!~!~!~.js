d12facba564cc6ae1ea3ff79fe531d6659558ba6
DHoops <dmhoops@gmail.com>
2019-11-26 17:29:07 -0500

    Add a pending inspection when one fails

--- Event/IRSA;BUILDING!~!~!~.js
+++ Event/IRSA;BUILDING!~!~!~.js
@@ -2,8 +2,13 @@ if(matches(inspResult,"Passed","Approved","Partial")){
     editAppSpecific("Permit Expiration Date", dateAdd(null,180));
 	scheduleInspectionsIRSA();
 }
+if (matches(inspResult, "Passed", "Approved")) {
+	createPendingInspection(inspGroup, inspType);
+}
 BD_Failed_Penalty_Inspection();
 
 if(aa.env.getValue("From") != "AA"){
     notifyContactInspResult("Applicant");
 }
+
+

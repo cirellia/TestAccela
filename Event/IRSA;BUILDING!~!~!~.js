20c6cf614aecc1414f1fef8eaa26405ffa260a6f
DHoops <dmhoops@gmail.com>
2019-12-02 08:28:02 -0500

    Change condition for pending insp

--- Event/IRSA;BUILDING!~!~!~.js
+++ Event/IRSA;BUILDING!~!~!~.js
@@ -2,7 +2,7 @@ if(matches(inspResult,"Passed","Approved","Partial")){
     editAppSpecific("Permit Expiration Date", dateAdd(null,180));
 	scheduleInspectionsIRSA();
 }
-if (matches(inspResult, "Passed", "Approved")) {
+if (!matches(inspResult, "Passed", "Approved")) {
 	createPendingInspection(inspGroup, inspType);
 }
 BD_Failed_Penalty_Inspection();

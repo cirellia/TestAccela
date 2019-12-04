0863246b77fa1b5554ffa4c92d65a35a3e04f9d6
DHoops <dmhoops@gmail.com>
2019-12-04 14:11:17 -0500

    add creation of pending inspections on failure result.

--- Event/IRSA;BUILDING!BUILDING!~!~.js
+++ Event/IRSA;BUILDING!BUILDING!~!~.js
@@ -1,2 +1,14 @@
 
-  BD_Electrical_Clearance();      
+if(matches(inspResult,"Passed","Approved","Partial")){
+    editAppSpecific("Permit Expiration Date", dateAdd(null,180));
+	scheduleInspectionsIRSA();
+}
+if (!matches(inspResult, "Passed", "Approved")) {
+	createPendingInspection(inspGroup, inspType);
+}
+BD_Failed_Penalty_Inspection();
+
+if(aa.env.getValue("From") != "AA"){
+    notifyContactInspResult("Applicant");
+}
+    
